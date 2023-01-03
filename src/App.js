471896273024a0e2991bb88b313c69492d52f7d4
Chris Harper <chris.harper@homeserve.com>
2023-01-03 10:48:55 +0000

    End of chapter 2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const message = "Hello, World";
+  const name = "Chris";
+
+  const handleStringChange = () => {
+      const strings = ['dog','cat','mouse'];
+      const int = Math.floor(Math.random() * 3);
+      return strings[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see changes.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Hello, {handleStringChange()}! {/*this is how you call a function in JSX*/}
+   
+        <p>{message}</p>
+        {/* this is how you do a comment in JSX. Remember to wrap the comment in a JavaScript expression*/}
       </header>
+
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; //Each component gets it's own file and as we are using <App/> We need to import it. 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
