acb83e6387fd21b124488d8c1377f9ff99d74696
Chris Harper <chris.harper@homeserve.com>
2023-01-03 13:54:50 +0000

    Added some maps

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,52 @@
 import React from 'react' //Component import
 import { useState } from 'react'; //Hook import
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
 
-  const [name, setName] = useState('Chris');
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+        id: 1,
+        checked: true,
+        desc: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+        id: 2,
+        checked: false,
+        desc: "Item 2"
+    },
+    {
+        id: 3,
+        checked: false,
+        desc: "Item 3"
+    }
+  ]);
+
+  const handleCheck = (id) => {
+    console.log(`key: ${id}`);
+  }
  
-  const handleNameChange = () => {
-      const names = ['dog','cat','mouse'];
-      const int = Math.floor(Math.random() * 3);
-      setName(names[int]);
-  }
-
-  const handleClick = () => {
-    setCount(count + 3);
-    console.log(count);
-  }
-
-  const handleClick2 = () => {
-    console.log(count);
-  }
-
+ 
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>Change name</button>
-        <button onClick={handleClick}>Click it</button>
-        <button onClick={handleClick2}>Click it</button>
+        <ul>
+          {items.map((item) => (
+
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label>{item.desc}</label>
+              <FaTrashAlt 
+                role="button" 
+                tabIndex="0"/>
+            </li>
+
+          ))}
+        </ul>
     </main>
   )
 }
